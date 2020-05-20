56380a0d8ceb2fad44d2f9c10915a44c5b6ee90b
Christoph Schweppe <info@cschweppe.de>
2020-05-20 19:07:14 +0200

    Update BlueThemeServiceProvider.php

--- src/BlueThemeServiceProvider.php
+++ src/BlueThemeServiceProvider.php
@@ -3,13 +3,13 @@
 namespace Emptynick\BlueTheme;
 
 use Illuminate\Support\ServiceProvider;
-use Voyager\Admin\Facades\Plugins as PluginFacade;
+use Voyager\Admin\Manager\Plugins as PluginManager;
 
 class BlueThemeServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(PluginManager $pluginmanager)
     {
-        PluginFacade::addPlugin(\Emptynick\BlueTheme\BlueTheme::class);
+        $pluginmanager->addPlugin(\Emptynick\BlueTheme\BlueTheme::class);
     }
 
     public function register()
